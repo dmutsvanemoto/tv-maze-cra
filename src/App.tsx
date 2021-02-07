35b3d99c436f365ff58fd8b7327708b8c318ef62
Daniel M <13464314+dmutsvanemoto@users.noreply.github.com>
2021-02-07 14:37:09 +0000

    Added routing to enable episode viewing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { HomePage } from "./pages/Home/HomePage";
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route path="/shows/:id"></Route>
+      </Switch>
+    </Router>
   );
 }
 
