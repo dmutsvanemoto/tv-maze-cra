45672ae22946eab767c5c0315c02bd969cd036eb
Daniel M <13464314+dmutsvanemoto@users.noreply.github.com>
2021-02-07 16:36:57 +0000

    resolve package warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,14 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: ['react', '@typescript-eslint', 'jest'],
-  extends: [
-    'airbnb-typescript',
-    'airbnb/hooks',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:jest/recommended',
-    'prettier',
-    'prettier/react',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended'
-  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  extends: ["airbnb-typescript-prettier"],
   env: {
     browser: true,
     es6: true,
     jest: true,
   },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-    project: './tsconfig.json',
-  },
-}
+};
